1351125804b08b6026d68df6e1335a4c90a015d7
aryan-marthak <aryan.marthak1@gmail.com>
2024-05-26 10:26:07 +0530

    9. Palindrome Number

---
+++ 9.py
@@ -0,0 +1,11 @@
+# 9. Palindrome Number
+# Given an integer x, return true if x is a palindrome, and false otherwise.
+
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        num = str(x)
+        return(bool(str(num)[::-1] == num))
