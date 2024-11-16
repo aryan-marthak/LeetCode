36a99d0052a17dfcaa7ab9bbc300eb7e1762255c
aryan-marthak <aryan.marthak1@gmail.com>
2024-11-16 23:56:33 +0530

    206. Reverse Linked List
    itterative way of doing using two pointer

---
+++ EASY/206.py
@@ -0,0 +1,21 @@
+# 206. Reverse Linked List
+
+# Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reverseList(self, head):
+        prev, curr = None, head
+
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        return prev
+        
