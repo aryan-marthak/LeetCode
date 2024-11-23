c8abaac169f2b70cc0d1634c20800441c4964192
aryan-marthak <aryan.marthak1@gmail.com>
2024-11-23 23:58:12 +0530

    242. Valid Anagram

---
+++ EASY/242.py
@@ -0,0 +1,10 @@
+# 242. Valid Anagram
+
+# Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s = sorted(s)
+        t = sorted(t)
+
+        return s == t
