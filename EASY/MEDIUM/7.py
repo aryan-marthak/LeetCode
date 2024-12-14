9fd350c3c81580c7b3695310ff30856fadca183c
aryan-marthak <aryan.marthak1@gmail.com>
2024-12-14 17:07:59 +0530

    a better easy fast and readable approach

--- EASY/MEDIUM/7.py
+++ EASY/MEDIUM/7.py
@@ -20,3 +20,15 @@ class Solution(object):
             return 0
         else:
             return rev * sgn
+
+
+# EASY AND READABLE APPROACH
+
+class Solution:
+    def reverse(self, x: int) -> int: 
+        s = str(abs(x))      
+        reversed = int(s[::-1])
+
+        if reversed > 2147483647:
+            return 0
+        return reversed if x > 0 else (reversed * -1)
