55998e9fc1efab467f7f8404344bb937a406fe5b
aryan-marthak <aryan.marthak1@gmail.com>
2024-12-26 16:06:35 +0530

    367. Valid Perfect Square

---
+++ EASY/367.py
@@ -0,0 +1,16 @@
+# 367. Valid Perfect Square
+
+# Given a positive integer num, return true if num is a perfect square or false otherwise.
+
+# A perfect square is an integer that is the square of an integer. In other words, it is the product of some integer with itself.
+
+# You must not use any built-in library function, such as sqrt.
+
+# not the best
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        x = num**0.5
+        if x == int(x):
+            return True
+        else: 
+            return False
