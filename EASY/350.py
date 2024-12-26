7aa9f9fc9bd9ee857a3cd9b297e90f1b0965935b
aryan-marthak <aryan.marthak1@gmail.com>
2024-12-26 15:54:47 +0530

    follow up solution

--- EASY/350.py
+++ EASY/350.py
@@ -22,3 +22,21 @@ class Solution:
                 output.append(n)
                 c[n] -= 1
         return output 
+    
+# follow up - already sorted arrays
+
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        i,j = 0,0
+        output = []
+        nums1, nums2 = sorted(nums1), sorted(nums2)
+        while i<len(nums1) and j<len(nums2):
+            if nums1[i]<nums2[j]:
+                i += 1
+            elif nums1[i]>nums2[j]:
+                j += 1
+            else:
+                output.append(nums1[i])
+                i+=1
+                j+=1
+        return output
