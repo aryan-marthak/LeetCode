1566d8e37dde9a1a4fd0782c633073451f5bf40d
aryan-marthak <aryan.marthak1@gmail.com>
2025-01-04 21:40:25 +0530

    434. Number of Segments in a String

---
+++ EASY/434.py
@@ -0,0 +1,10 @@
+# 434. Number of Segments in a String
+
+# Given a string s, return the number of segments in the string.
+
+# A segment is defined to be a contiguous sequence of non-space characters.
+
+class Solution:
+    def countSegments(self, s: str) -> int:
+        s = s.split()
+        return len(s)
