1b2bc28dff773702c67b7d551c3f29de88e4fff8
aryan-marthak <aryan.marthak1@gmail.com>
2025-02-17 02:36:04 +0530

    53. Maximum Subarray (KADANE's ALGORITHM)

---
+++ MEDIUM/53.py
@@ -0,0 +1,28 @@
+# 53. Maximum Subarray
+
+# Given an integer array nums, find the
+# subarray
+# with the largest sum, and return its sum.
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+
+        sum = 0
+        maxi = float('-inf')
+
+        for i in range(len(nums)):
+            sum += nums[i]
+            if sum > maxi:
+                maxi = sum
+            if sum < 0:
+                sum = 0
+
+        return maxi
+        
+        # maxi = min(nums)
+        # for i in range(len(nums)):
+        #     total = 0
+        #     for j in range(i, len(nums)):
+        #         total += nums[j]
+        #         maxi = max(maxi, total)
+        # return maxi
