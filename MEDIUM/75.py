c65a44c939f3876a72d3735f931bd225247c4e75
aryan-marthak <aryan.marthak1@gmail.com>
2025-02-18 00:54:10 +0530

    75. Sort Colors
    Bubble Sort (not the most optimal approach)

---
+++ MEDIUM/75.py
@@ -0,0 +1,18 @@
+# 75. Sort Colors
+
+# Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+
+# We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+
+# You must solve this problem without using the library's sort function.
+
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+
+        # bubble sort
+
+
+        for i in range(len(nums) - 1, -1, -1):
+            for j in range(i):
+                if nums[j] > nums[j + 1]:
+                    nums[j], nums[j + 1] = nums[j + 1], nums[j]
