2ccd90eaf58e84e5eda1c8f67355dd51861e8679
aryan-marthak <aryan.marthak1@gmail.com>
2025-02-28 00:01:32 +0530

    451. Sort Characters By Frequency

---
+++ MEDIUM/451.py
@@ -0,0 +1,22 @@
+# 451. Sort Characters By Frequency
+
+# Given a string s, sort it in decreasing order based on the frequency of the characters. The frequency of a character is the number of times it appears in the string.
+
+# Return the sorted string. If there are multiple answers, return any of them.
+
+# bucket sort solution
+
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        count = Counter(s)
+        buckets = defaultdict(list)
+
+        for char, cnt in count.items():
+            buckets[cnt].append(char)
+
+        res = ""
+        for i in range(len(s), 0, -1):
+            for c in buckets[i]:
+                res += c * i
+
+        return res
