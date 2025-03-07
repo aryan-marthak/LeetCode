e08ada89a709d586d203e917a0335ee0422ba564
aryan-marthak <aryan.marthak1@gmail.com>
2025-03-07 23:36:13 +0530

    86. Partition List

---
+++ MEDIUM/86.py
@@ -0,0 +1,27 @@
+# 86. Partition List
+
+# Given the head of a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.
+
+# You should preserve the original relative order of the nodes in each of the two partitions.
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+        left, right = ListNode(), ListNode()
+        ltail, rtail = left, right
+
+        while head:
+            if head.val < x:
+                ltail.next = head
+                ltail = ltail.next
+            else:
+                rtail.next = head
+                rtail = rtail.next
+            head = head.next
+        ltail.next = right.next
+        rtail.next = None
+        return left.next
