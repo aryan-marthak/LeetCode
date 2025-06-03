c9eab68a94e62e64e2fa81b6301e888c97f1f824
aryan-marthak <aryan.marthak1@gmail.com>
2025-06-03 15:59:49 +0530

    Update 1768.py

--- EASY/1768.py
+++ EASY/1768.py
@@ -22,3 +22,16 @@ class Solution:
             j += 1
         return "".join(l)
             
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        i, j = 0, 0
+        l = []
+        while i < len(word1) and j < len(word2):
+            l.append(word1[i])
+            l.append(word2[j])
+            i += 1
+            j += 1
+        
+        l.append(word1[i:])
+        l.append(word2[j:])
+        return "".join(l)
