de1ea242673b6220a352a92fc987bc26248ec772
aryan-marthak <aryan.marthak1@gmail.com>
2025-06-03 14:43:43 +0530

    Update 88.py

--- EASY/88.py
+++ EASY/88.py
@@ -20,3 +20,22 @@ class Solution(object):
         while n > 0:
             nums1[last] = nums2[n - 1]
             n, last = n - 1, last - 1
+            
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        a = m - 1
+        b = n - 1
+        c = len(nums1) - 1
+        while a >= 0 and b >= 0:
+            if nums1[a] > nums2[b]:
+                nums1[c] = nums1[a]
+                a -= 1
+            else:
+                nums1[c] = nums2[b]
+                b -= 1
+            c -= 1
+        while b >= 0:
+            nums1[c] = nums2[b]
+            b -= 1 
+            c -= 1
