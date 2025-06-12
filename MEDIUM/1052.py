fcebae4ad26f8902023186a6562f13dfbdfefbfc
aryan-marthak <aryan.marthak1@gmail.com>
2025-06-12 10:20:56 +0530

    1052. Grumpy Bookstore Owner

---
+++ MEDIUM/1052.py
@@ -0,0 +1,33 @@
+# 1052. Grumpy Bookstore Owner
+
+# There is a bookstore owner that has a store open for n minutes. You are given an integer array customers of length n where customers[i] is the number of the customers that enter the store at the start of the ith minute and all those customers leave after the end of that minute.
+
+# During certain minutes, the bookstore owner is grumpy. You are given a binary array grumpy where grumpy[i] is 1 if the bookstore owner is grumpy during the ith minute, and is 0 otherwise.
+
+# When the bookstore owner is grumpy, the customers entering during that minute are not satisfied. Otherwise, they are satisfied.
+
+# The bookstore owner knows a secret technique to remain not grumpy for minutes consecutive minutes, but this technique can only be used once.
+
+# Return the maximum number of customers that can be satisfied throughout the day.
+
+# SLIDING WINDOW
+
+class Solution:
+    def maxSatisfied(self, customers: List[int], grumpy: List[int], minutes: int) -> int:
+        l = 0
+        window, Max = 0, 0
+        satisfied = 0
+        for r in range(len(customers)):
+            if grumpy[r]:
+                window += customers[r]
+            else:
+                satisfied += customers[r]
+            
+            if r - l + 1 > minutes:
+                if grumpy[l]:
+                    window -= customers[l]
+                l += 1
+            Max = max(window, Max)
+        return satisfied + Max
+    
+    
