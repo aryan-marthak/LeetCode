d04a079f845529486900cbeee174d7a156bf085b
aryan-marthak <aryan.marthak1@gmail.com>
2025-06-17 15:57:00 +0530

    used stack

--- EASY/1544.py
+++ EASY/1544.py
@@ -26,3 +26,19 @@ class Solution:
                 i -= 2
             i += 1
         return s 
+
+# STACK Approach
+
+class Solution:
+    def makeGood(self, s: str) -> str:
+        stack = []
+        i = 0
+        while i < len(s):
+            if stack and s[i] != stack[-1] and s[i].lower() == stack[-1].lower():
+                stack.pop()
+            else:
+                stack.append(s[i])
+            i += 1
+        
+        return "".join(stack)
+    
