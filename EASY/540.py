8de0225c5426c5b4447ee6fda37341ae159ee25c
Aryan Marthak <aryan.marthak1@gmail.com>
2025-07-09 23:48:30 +0530

    used binary search

--- EASY/540.py
+++ EASY/540.py
@@ -17,3 +17,19 @@ class Solution:
             ):
                 continue
             return nums[i]
+        
+# BINARY SEARCH
+
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = l + ((r - l) // 2)
+            if ((m - 1 < 0 or nums[m - 1] != nums[m]) and 
+                (m + 1 == len(nums) or nums[m + 1] != nums[m])):
+                return nums[m]
+            lsize = m - 1 if nums[m - 1] == nums[m] else m
+            if lsize % 2:
+                r = m - 1
+            else: 
+                l = m + 1
