bea4aa7397092986c736a96ada5893b16012987a
Aryan Marthak <aryan.marthak1@gmail.com>
2025-07-21 00:01:25 +0530

    Update 2415.py

--- MEDIUM/2415.py
+++ MEDIUM/2415.py
@@ -22,6 +22,13 @@ class Solution:
 
         i = 0
         while q:
+            if i & 1:
+                l, r = 0, len(q) - 1
+                while l < r:
+                    q[l].val, q[r].val = q[r].val, q[l].val 
+                    l += 1
+                    r -= 1
+
             for _ in range(len(q)):
                 node = q.popleft()
                 if node.left:
