786ef2433bf3086b31997a9f45a2f18da7605f3e
Aryan Marthak <aryan.marthak1@gmail.com>
2025-07-23 00:00:43 +0530

    394. Find Lucky Integer in an Array

---
+++ EASY/1394.py
@@ -0,0 +1,22 @@
+# 1394. Find Lucky Integer in an Array
+
+# Given an array of integers arr, a lucky integer is an integer that has a frequency in the array equal to its value.
+
+# Return the largest lucky integer in the array. If there is no lucky integer return -1.
+
+class Solution:
+    def findLucky(self, arr: List[int]) -> int:
+
+        # BRUTE FORCE APPROACH
+
+        comp = -1
+        for i in arr:
+            temp = i
+            res = 0
+            for j in arr:
+                if j == temp:
+                    res += 1
+            if res == temp:
+                comp = max(comp, temp)
+        return comp
+
