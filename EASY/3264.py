8bed19d2874bfd03ad80d18992d3fe555e71b04b
Aryan Marthak <aryan.marthak1@gmail.com>
2025-08-25 02:24:41 +0530

    3264 MINHEAP

--- EASY/3264.py
+++ EASY/3264.py
@@ -11,6 +11,22 @@
 
 class Solution:
     def getFinalState(self, nums: List[int], k: int, multiplier: int) -> List[int]:
+        # MINHEAP
+
+        res = nums[:]
+
+        min_heap = [(num, i) for i, num in enumerate(nums)]
+        heapq.heapify(min_heap)
+
+        for i in range(k):
+            num, i = heapq.heappop(min_heap)
+            res[i] *= multiplier
+            heapq.heappush(min_heap, (res[i], i)) 
+
+        return res
+        
+        # SIMULATION
+        
         for i in range(k):
             temp = 0
             for j in range(1, len(nums)):
