4eb6b668e19f69891d4686b7b164e5baf115a5cd
Aryan Marthak <aryan.marthak1@gmail.com>
2025-10-13 03:06:37 +0530

    Update 50.py

--- MEDIUM/50.py
+++ MEDIUM/50.py
@@ -2,15 +2,26 @@
 
 # Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
 
-# WRONG SOLUTION (MAX RECURSION DEPTH EXCEEDED)
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        if n == 0:
-            return 1
-        if n < 0:
-            x = 1/x
-            n = -n
-        if n > 0:
-            temp = self.myPow(x, n - 1)
-            return temp * x
-        
+        def helper(x, n):
+            if x == 0: return 0
+            if n == 0: return 1
+
+            res = helper(x, n // 2)
+            res *= res
+            return x * res if n % 2 else res
+        res = helper(x, abs(n))
+        return res if n >= 0 else 1 / res
+
+# WRONG SOLUTION (MAX RECURSION DEPTH EXCEEDED)
+# class Solution:
+#     def myPow(self, x: float, n: int) -> float:
+#         if n == 0:
+#             return 1
+#         if n < 0:
+#             x = 1/x
+#             n = -n
+#         if n > 0:
+#             temp = self.myPow(x, n - 1)
+#             return temp * x
