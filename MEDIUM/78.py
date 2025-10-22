0aa0027f038d2c1f10f54ad7a099f2d443f76be4
Aryan Marthak <aryan.marthak1@gmail.com>
2025-10-22 15:44:33 +0530

    used bactracking

--- MEDIUM/78.py
+++ MEDIUM/78.py
@@ -4,6 +4,19 @@
 
 # The solution set must not contain duplicate subsets. Return the solution in any order.
 
+# BACKTRACKING
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        def backtrack(index, Set):
+            res.append(Set[:])
+            for i in range(index, len(nums)):
+                Set.append(nums[i])
+                backtrack(i + 1, Set)
+                Set.pop()
+        backtrack(0, [])
+        return res
+
 # ITERATIVE APPROACH
 
 class Solution:
