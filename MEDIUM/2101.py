dc88a095a8de4562a582f42040826adf754c95d7
Aryan Marthak <aryan.marthak1@gmail.com>
2025-11-30 17:40:13 +0530

    2101. Detonate the Maximum Bombs

---
+++ MEDIUM/2101.py
@@ -0,0 +1,36 @@
+# 2101. Detonate the Maximum Bombs
+
+# You are given a list of bombs. The range of a bomb is defined as the area where its effect can be felt. This area is in the shape of a circle with the center as the location of the bomb.
+
+# The bombs are represented by a 0-indexed 2D integer array bombs where bombs[i] = [xi, yi, ri]. xi and yi denote the X-coordinate and Y-coordinate of the location of the ith bomb, whereas ri denotes the radius of its range.
+
+# You may choose to detonate a single bomb. When a bomb is detonated, it will detonate all bombs that lie in its range. These bombs will further detonate the bombs that lie in their ranges.
+
+# Given the list of bombs, return the maximum number of bombs that can be detonated if you are allowed to detonate only one bomb.
+
+class Solution:
+    def maximumDetonation(self, bombs: List[List[int]]) -> int:
+        lst = collections.defaultdict(list)
+        for i in range(len(bombs)):
+            for j in range(i + 1, len(bombs)):
+                x1, y1, r1 = bombs[i]
+                x2, y2, r2 = bombs[j]
+                d = sqrt((x1 - x2)**2 + (y1 - y2)**2)
+
+                if d <= r1:
+                    lst[i].append(j)
+                if d <= r2:
+                    lst[j].append(i)
+
+        def dfs(i, visit):
+            if i in visit:
+                return 0
+            visit.add(i)
+            for x in lst[i]:
+                dfs(x, visit)
+            return len(visit)
+        
+        res = 0
+        for i in range(len(bombs)):
+            res = max(res, dfs(i, set()))
+        return res
