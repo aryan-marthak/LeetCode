0656b29a69d499b89e08e0b59daa3a935acb115e
Aryan Marthak <aryan.marthak1@gmail.com>
2025-12-03 15:13:16 +0530

    Update 416.py

--- MEDIUM/416.py
+++ MEDIUM/416.py
@@ -2,6 +2,7 @@
 
 # Given an integer array nums, return true if you can partition the array into two subsets such that the sum of the elements in both subsets is equal or false otherwise.
 
+# BRUTE FORCE APPROACH
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
         if sum(nums) % 2 != 0:
@@ -20,3 +21,19 @@ class Solution:
                 return True
         return False
     
+# DP APPROACH
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        if sum(nums) % 2 != 0:
+            return False
+        target = sum(nums) // 2
+
+        dp = set()
+        dp.add(0)
+        for i in range(len(nums) - 1, -1, -1):
+            newdp = set()
+            for j in dp:
+                newdp.add(j + nums[i])
+                newdp.add(j)
+            dp = newdp
+        return True if target in dp else False
