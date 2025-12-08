27e67eccb373b663a54d7dc1e61afb90bc059602
Aryan Marthak <aryan.marthak1@gmail.com>
2025-12-09 01:15:06 +0530

    Update 64.py

--- MEDIUM/64.py
+++ MEDIUM/64.py
@@ -19,3 +19,16 @@ class Solution:
             dp[(r, c)] = grid[r][c] + min(dfs(r + 1, c), dfs(r, c + 1))
             return dp[(r, c)]
         return dfs(0, 0)
+    
+# DYNAMIC PROGRAMMING (BOTTOM-UP DP)
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
+        dp = [[float("inf")] * (COLS + 1) for _ in range(ROWS + 1)]
+        dp[ROWS - 1][COLS] = 0
+
+        for r in range(ROWS - 1, -1, -1):
+            for c in range(COLS - 1, -1, -1):
+                dp[r][c] = grid[r][c] + min(dp[r + 1][c], dp[r][c + 1])
+
+        return dp[0][0]
