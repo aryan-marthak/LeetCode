53d7d18e8e98d23dd39533c2ccf44d81bec01437
Aryan Marthak <aryan.marthak1@gmail.com>
2025-12-17 00:01:03 +0530

    Update 1614.py

--- EASY/1614.py
+++ EASY/1614.py
@@ -2,6 +2,20 @@
 
 # Given a valid parentheses string s, return the nesting depth of s. The nesting depth is the maximum number of nested parentheses.
 
+# STACK
+class Solution:
+    def maxDepth(self, s: str) -> int:
+        res, stack = 0, []
+
+        for c in s:
+            if c == "(":
+                stack.append(c)
+                res = max(res, len(stack))
+            elif c == ")":
+                stack.pop()
+
+        return res
+
 # ITERATIVE
 class Solution:
     def maxDepth(self, s: str) -> int:
