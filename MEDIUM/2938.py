46624a66c33f62e61d9b92d9579b8f3dc6b8c811
Aryan Marthak <aryan.marthak1@gmail.com>
2025-12-17 18:51:21 +0530

    2938. Separate Black and White Balls

---
+++ MEDIUM/2938.py
@@ -0,0 +1,19 @@
+# 2938. Separate Black and White Balls
+
+# There are n balls on a table, each ball has a color black or white.
+
+# You are given a 0-indexed binary string s of length n, where 1 and 0 represent black and white balls, respectively.
+
+# In each step, you can choose two adjacent balls and swap them.
+
+# Return the minimum number of steps to group all the black balls to the right and all the white balls to the left.
+
+class Solution:
+    def minimumSteps(self, s: str) -> int:
+        l = 0
+        res = 0
+        for i in range(len(s)):
+            if s[i] == "0":
+                res += (i - l)
+                l += 1
+        return res
