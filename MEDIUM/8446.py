95ebfc9fb2c308b33828dc07b73448fc78d206c4
Aryan Marthak <aryan.marthak1@gmail.com>
2025-12-21 01:33:37 +0530

    846. Hand of Straights

---
+++ MEDIUM/8446.py
@@ -0,0 +1,20 @@
+# 846. Hand of Straights
+
+# Alice has some number of cards and she wants to rearrange the cards into groups so that each group is of size groupSize, and consists of groupSize consecutive cards.
+
+# Given an integer array hand where hand[i] is the value written on the ith card and an integer groupSize, return true if she can rearrange the cards, or false otherwise.
+
+class Solution:
+    def isNStraightHand(self, hand, groupSize):
+        if len(hand) % groupSize:
+            return False
+
+        count = Counter(hand)
+        hand.sort()
+        for num in hand:
+            if count[num]:
+                for i in range(num, num + groupSize):
+                    if not count[i]:
+                        return False
+                    count[i] -= 1
+        return True
