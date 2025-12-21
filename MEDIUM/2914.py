5dd6142e6f175ff25bdf3467dc100141a08ab547
Aryan Marthak <aryan.marthak1@gmail.com>
2025-12-22 00:21:12 +0530

    Update 2914.py

--- MEDIUM/2914.py
+++ MEDIUM/2914.py
@@ -13,6 +13,14 @@
 
 class Solution:
     def minChanges(self, s: str) -> int:
+        res = 0
+        for i in range(0, len(s), 2):
+            if s[i] != s[i + 1]:
+                res += 1
+        return res
+
+class Solution:
+    def minChanges(self, s: str) -> int:
         l = 0
         res = 0
         for i in range(len(s)):
