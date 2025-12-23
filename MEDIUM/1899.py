ab5d4847dd093a41800b7e7c1a1935db1dd30d9d
Aryan Marthak <aryan.marthak1@gmail.com>
2025-12-23 23:52:40 +0530

    1899. Merge Triplets to Form Target Triplet

---
+++ MEDIUM/1899.py
@@ -0,0 +1,21 @@
+# 1899. Merge Triplets to Form Target Triplet
+
+# A triplet is an array of three integers. You are given a 2D integer array triplets, where triplets[i] = [ai, bi, ci] describes the ith triplet. You are also given an integer array target = [x, y, z] that describes the triplet you want to obtain.
+
+# To obtain target, you may apply the following operation on triplets any number of times (possibly zero):
+
+#     Choose two indices (0-indexed) i and j (i != j) and update triplets[j] to become [max(ai, aj), max(bi, bj), max(ci, cj)].
+#         For example, if triplets[i] = [2, 5, 3] and triplets[j] = [1, 7, 5], triplets[j] will be updated to [max(2, 1), max(5, 7), max(3, 5)] = [2, 7, 5].
+
+# Return true if it is possible to obtain the target triplet [x, y, z] as an element of triplets, or false otherwise.
+
+class Solution:
+    def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
+        Set = set()
+        for i in triplets:
+            if i[0] > target[0] or i[1] > target[1] or i[2] > target[2]:
+                continue
+            for p, q in enumerate(i):
+                if q == target[p]:
+                    Set.add(p)
+        return len(Set) == 3
