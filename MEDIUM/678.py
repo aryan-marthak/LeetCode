6fda0744b3cb2b937d860e207b1ef9c5663d5d2a
Aryan Marthak <aryan.marthak1@gmail.com>
2025-12-24 01:27:29 +0530

    678. Valid Parenthesis String

---
+++ MEDIUM/678.py
@@ -0,0 +1,28 @@
+# 678. Valid Parenthesis String
+
+# Given a string s containing only three types of characters: '(', ')' and '*', return true if s is valid.
+
+# The following rules define a valid string:
+
+#     Any left parenthesis '(' must have a corresponding right parenthesis ')'.
+#     Any right parenthesis ')' must have a corresponding left parenthesis '('.
+#     Left parenthesis '(' must go before the corresponding right parenthesis ')'.
+#     '*' could be treated as a single right parenthesis ')' or a single left parenthesis '(' or an empty string "".
+
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        def dfs(i, open):
+            if open < 0:
+                return False
+            if i == len(s):
+                return open == 0
+
+            if s[i] == '(':
+                return dfs(i + 1, open + 1)
+            elif s[i] == ')':
+                return dfs(i + 1, open - 1)
+            else:
+                return (dfs(i + 1, open) or
+                        dfs(i + 1, open + 1) or
+                        dfs(i + 1, open - 1))
+        return dfs(0, 0)
