581d2e9e8d03785dc8cb5fa2fbaa21e460bc4eff
Aryan Marthak <aryan.marthak1@gmail.com>
2025-12-24 18:23:39 +0530

    Update 678.py

--- MEDIUM/678.py
+++ MEDIUM/678.py
@@ -9,6 +9,7 @@
 #     Left parenthesis '(' must go before the corresponding right parenthesis ')'.
 #     '*' could be treated as a single right parenthesis ')' or a single left parenthesis '(' or an empty string "".
 
+# RECURSION WITH BACKTRACKING
 class Solution:
     def checkValidString(self, s: str) -> bool:
         def dfs(i, open):
@@ -25,4 +26,27 @@ class Solution:
                 return (dfs(i + 1, open) or
                         dfs(i + 1, open + 1) or
                         dfs(i + 1, open - 1))
+        return dfs(0, 0)
+
+# MEMOIZATION TOP-DOWN DP
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        dp = {}
+        def dfs(i, open):
+            if open < 0:
+                return False
+            if (i, open) in dp:
+                return dp[(i, open)]
+            if i == len(s):
+                return open == 0
+            if s[i] == '(':
+                res = dfs(i + 1, open + 1)
+            elif s[i] == ')':
+                res = dfs(i + 1, open - 1)
+            else:
+                res = (dfs(i + 1, open) or
+                        dfs(i + 1, open + 1) or
+                        dfs(i + 1, open - 1))
+            dp[(i, open)] = res
+            return res
         return dfs(0, 0)
