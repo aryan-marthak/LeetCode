0a8301d85882f9f2eb81037401ddf7066990e557
Aryan Marthak <aryan.marthak1@gmail.com>
2025-12-26 23:55:37 +0530

    135. Candy

---
+++ HARD/135.py
@@ -0,0 +1,25 @@
+# 135. Candy
+
+# There are n children standing in a line. Each child is assigned a rating value given in the integer array ratings.
+
+# You are giving candies to these children subjected to the following requirements:
+
+#     Each child must have at least one candy.
+#     Children with a higher rating get more candies than their neighbors.
+
+# Return the minimum number of candies you need to have to distribute the candies to the children.
+
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        n = len(ratings)
+        arr = [1] * n
+
+        for i in range(1, n):
+            if ratings[i - 1] < ratings[i]:
+                arr[i] = arr[i - 1] + 1
+
+        for i in range(n - 2, -1, -1):
+            if ratings[i] > ratings[i + 1]:
+                arr[i] = max(arr[i], arr[i + 1] + 1)
+
+        return sum(arr)
