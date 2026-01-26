1f6fa68a5dd8da79b711461abce0a3cba99c068c
Aryan Marthak <aryan.marthak1@gmail.com>
2026-01-26 23:05:56 +0530

    Update 226.py

--- BLIND 75/226.py
+++ BLIND 75/226.py
@@ -8,6 +8,24 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        def helper(node):
+            if not node:
+                return
+            node.left, node.right = node.right, node.left
+            helper(node.left)
+            helper(node.right)
+        helper(root)
+        return root
+
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         if not root:
