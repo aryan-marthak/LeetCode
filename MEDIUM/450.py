6812a48b83414430cffdf28e73ac15f08ddf83b3
Aryan Marthak <aryan.marthak1@gmail.com>
2026-04-03 10:38:25 +0530

    Update 450.py

--- MEDIUM/450.py
+++ MEDIUM/450.py
@@ -16,6 +16,39 @@
 #         self.right = right
 class Solution:
     def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        # DETAILED EXPLANATION:
+        
+        if not root:
+            return None
+        if root.val == key:
+            return self.helper(root)
+        cur = root
+        while root:
+            if key < root.val:
+                if root.left and root.left.val == key:
+                    root.left = self.helper(root.left)
+                    break
+                root = root.left
+            else:
+                if root.right and root.right.val == key:
+                    root.right = self.helper(root.right)
+                    break
+                root = root.right
+        return cur
+
+    def helper(self, root):
+        if root.left is None:
+            return root.right
+        if root.right is None:
+            return root.left
+        left = root.left
+        right = root.right
+        while left.right:
+            left = left.right
+        left.right = right
+        return root.left
+        
+        #  SIMPLER EXPLANATION:
         if not root:
             return root
 
