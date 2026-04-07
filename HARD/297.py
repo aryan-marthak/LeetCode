c6a50aec99b2102c57f4782c20dff976f5dba197
Aryan Marthak <aryan.marthak1@gmail.com>
2026-04-07 23:59:10 +0530

    297. Serialize and Deserialize Binary Tree

---
+++ HARD/297.py
@@ -0,0 +1,49 @@
+# 297. Serialize and Deserialize Binary Tree
+
+# Serialization is the process of converting a data structure or object into a sequence of bits so that it can be stored in a file or memory buffer, or transmitted across a network connection link to be reconstructed later in the same or another computer environment.
+
+# Design an algorithm to serialize and deserialize a binary tree. There is no restriction on how your serialization/deserialization algorithm should work. You just need to ensure that a binary tree can be serialized to a string and this string can be deserialized to the original tree structure.
+
+# Clarification: The input/output format is the same as how LeetCode serializes a binary tree. You do not necessarily need to follow this format, so please be creative and come up with different approaches yourself.
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Codec:
+
+    def serialize(self, root):
+        res = []
+
+        def dfs(node):
+            if not node:
+                res.append(str("N"))
+                return
+            res.append(str(node.val))
+            dfs(node.left)
+            dfs(node.right)
+        dfs(root)
+        return ",".join(res)
+
+    def deserialize(self, data):
+        values = data.split(",")
+        self.i = 0
+
+        def dfs():
+            if values[self.i] == "N":
+                self.i += 1
+                return None
+            node = TreeNode(int(values[self.i]))
+            self.i += 1
+            node.left = dfs()        
+            node.right = dfs()        
+            return node
+        return dfs()
+
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# ans = deser.deserialize(ser.serialize(root))
