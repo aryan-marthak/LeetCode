0d0c7561069492c1e697fbab93ad6427c0a8982f
Aryan Marthak <aryan.marthak1@gmail.com>
2026-05-09 18:03:47 +0530

    Update 463.py

--- EASY/463.py
+++ EASY/463.py
@@ -8,6 +8,8 @@
 
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
+        # Iterative
+        
         peri = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
@@ -18,3 +20,24 @@ class Solution:
                     if i > 0 and grid[i - 1][j] == 1:
                         peri -= 2
         return peri
+        
+        # DFS
+        
+        rows, cols = len(grid), len(grid[0])
+        visit = set()
+
+        def dfs(i, j):
+            if i < 0 or j < 0 or i >= rows or j >= cols or grid[i][j] == 0:
+                return 1
+            if (i, j) in visit:
+                return 0
+            
+            visit.add((i, j))
+            peri = dfs(i, j + 1) + dfs(i + 1, j) + dfs(i, j - 1) + dfs(i - 1, j)
+            return peri
+
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == 1:
+                    return dfs(i, j)
+        return 0
