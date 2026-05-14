e7e081a76192f38ce3601c59bddfc1322a652c88
Aryan Marthak <aryan.marthak1@gmail.com>
2026-05-15 00:13:32 +0530

    Update 70.py

--- EASY/70.py
+++ EASY/70.py
@@ -5,6 +5,8 @@
 # Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
 
 class Solution(object):
+    # Fibonacci Sequence
+
     def climbStairs(self, n):
         one, two = 1, 1 
 
@@ -14,3 +16,10 @@ class Solution(object):
             two = temp
         return one
         
+    # RECURRSION
+
+        def dfs(i):
+            if i >= n:
+                return i == n
+            return dfs(i + 1) + dfs(i + 2)
+        return dfs(0)
