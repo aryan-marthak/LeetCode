ff844bbedf2fd1373b3c32613572a8dae7582643
Aryan Marthak <aryan.marthak1@gmail.com>
2026-05-16 20:21:45 +0530

    Update 70.py

--- EASY/70.py
+++ EASY/70.py
@@ -23,3 +23,16 @@ class Solution(object):
                 return i == n
             return dfs(i + 1) + dfs(i + 2)
         return dfs(0)
+    
+    # DP TOP DOWN 
+
+        cache = [-1] * n
+        def dfs(i):
+            if i >= n:
+                return i == n
+            if cache[i] != -1:
+                return cache[i]
+            cache[i] = dfs(i + 1) + dfs(i + 2)
+            return cache[i]
+
+        return dfs(0)
