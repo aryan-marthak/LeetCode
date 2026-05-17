0b990ddfb6c8ecbe97a02f29f3de291cbed3e5d0
Aryan Marthak <aryan.marthak1@gmail.com>
2026-05-17 20:00:46 +0530

    213. House Robber II

--- MEDIUM/213.py
+++ MEDIUM/213.py
@@ -16,3 +16,33 @@ class Solution:
         if len(nums) == 1:
             return nums[0]
         return max(helper(nums[1:]), helper(nums[:-1]))
+
+# DFS solution, TLE
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        def dfs(i, flag):
+            if i >= len(nums) or (flag and i == len(nums) - 1):
+                return 0
+
+            return max(dfs(i + 1, flag), nums[i] + dfs(i + 2, flag or i == 0))
+        return max(dfs(0, True), dfs(1, False))
+    
+# DFS with memoization
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+
+        memo = [[-1] * 2 for _ in range(len(nums))]
+
+        def dfs(i, flag):
+            if i >= len(nums) or (flag and i == len(nums) - 1):
+                return 0
+            if memo[i][flag] != -1:
+                return memo[i][flag]
+            memo[i][flag] = max(dfs(i + 1, flag), nums[i] + dfs(i + 2, flag or (i == 0)))
+            return memo[i][flag]
+
+        return max(dfs(0, True), dfs(1, False))
