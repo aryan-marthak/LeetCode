276c01771cd2b0308f3904167657515e8cd01e80
Aryan Marthak <aryan.marthak1@gmail.com>
2026-05-21 16:54:18 +0530

    Update 91.py

--- MEDIUM/91.py
+++ MEDIUM/91.py
@@ -28,6 +28,24 @@
 
 class Solution:
     def numDecodings(self, s: str) -> int:
+        # DP TOP DOWN
+        dp = {len(s) : 1}
+
+        def dfs(i):
+            if i in dp:
+                return dp[i]
+            if s[i] == "0":
+                return 0
+            
+            res = dfs(i + 1)
+            if i + 1 < len(s) and (s[i] == "1" or s[i] == "2" and s[i + 1] in "0123456"):
+                res += dfs(i + 2)
+            dp[i] = res
+            return res
+        return dfs(0)
+    
+class Solution:
+    def numDecodings(self, s: str) -> int:
         if s[0] == "0":
             return 0
         dp = [1, 1]
