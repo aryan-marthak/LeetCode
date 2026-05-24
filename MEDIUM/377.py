27e629892ae3a8b59861766b0e7a3f5c42a380c5
Aryan Marthak <aryan.marthak1@gmail.com>
2026-05-24 23:02:32 +0530

    377. Combination Sum IV

---
+++ MEDIUM/377.py
@@ -0,0 +1,16 @@
+# 377. Combination Sum IV
+
+# Given an array of distinct integers nums and a target integer target, return the number of possible combinations that add up to target.
+
+# The test cases are generated so that the answer can fit in a 32-bit integer.
+
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        dp = [0] * (target + 1)
+        dp[0] = 1
+
+        for i in range(target + 1):
+            for num in nums:
+                if i >= num:
+                    dp[i] += dp[i - num]
+        return dp[-1]
