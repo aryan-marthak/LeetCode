1c5ad5366f393dd8c75b3bf82a04d0cf9de63d3b
Aryan Marthak <aryan.marthak1@gmail.com>
2026-05-26 16:37:54 +0530

    Update 279.py

--- MEDIUM/279.py
+++ MEDIUM/279.py
@@ -4,6 +4,29 @@
 
 # A perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself. For example, 1, 4, 9, and 16 are perfect squares while 3 and 11 are not.
 
+# Top down approach with memoization
+class Solution:
+    def numSquares(self, n: int) -> int:
+        memo = {}
+
+        def dfs(target):
+            if target == 0:
+                return 0
+            if target in memo:
+                return memo[target]
+
+            res = target
+            for i in range(1, target + 1):
+                if i * i > target:
+                    break
+                res = min(res, 1 + dfs(target - i * i))
+
+            memo[target] = res
+            return res
+
+        return dfs(n)
+
+# Top down approach without memoization (inefficient) 
 class Solution:
         def numSquares(self, n: int) -> int:
             def dfs(n):
@@ -18,6 +41,8 @@ class Solution:
                 return temp
             return dfs(n)
         
+
+# Bottom up approach with tabulation
 class Solution:
     def numSquares(self, n: int) -> int:
         dp = [n] * (n + 1)
