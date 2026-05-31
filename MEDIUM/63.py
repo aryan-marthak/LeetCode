0e822a98b1a32329a7864801ebd73b8d82fc13ea
Aryan Marthak <aryan.marthak1@gmail.com>
2026-05-31 18:58:31 +0530

    Update 63.py

--- MEDIUM/63.py
+++ MEDIUM/63.py
@@ -36,3 +36,17 @@ class Solution:
             dp[(r, c)] = dfs(r + 1, c) + dfs(r, c + 1)
             return dp[(r, c)]
         return dfs(0, 0)
+
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        m, n = len(obstacleGrid), len(obstacleGrid[0])
+        dp = [0] * n
+        dp[n - 1] = 1
+
+        for r in reversed(range(m)):
+            for c in reversed(range(n)):
+                if obstacleGrid[r][c]:
+                    dp[c] = 0
+                elif c + 1 < n:
+                    dp[c] = dp[c] + dp[c + 1]
+        return dp[0]
