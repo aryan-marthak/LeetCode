29e9cc5e0f08a72a52cf53c530b5cf1e92a92f3f
Aryan Marthak <aryan.marthak1@gmail.com>
2026-06-05 18:33:16 +0530

    Create 72.py

---
+++ MEDIUM/72.py
@@ -0,0 +1,48 @@
+# 72. Edit Distance
+
+# Given two strings word1 and word2, return the minimum number of operations required to convert word1 to word2.
+
+# You have the following three operations permitted on a word:
+
+#     Insert a character
+#     Delete a character
+#     Replace a character
+
+# Recursion
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        m, n = len(word1), len(word2)
+
+        def dfs(i, j):
+            if i == m:
+                return n - j
+            if j == n:
+                return m - i
+            if word1[i] == word2[j]:
+                return dfs(i + 1, j + 1)
+            res = min(dfs(i + 1, j), dfs(i, j + 1))
+            res = min(res, dfs(i + 1, j + 1))
+            return res + 1
+        return dfs(0, 0)
+    
+# Recursion with memoization (Top-Down DP)
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        m, n = len(word1), len(word2)
+        dp = {}
+
+        def dfs(i, j):
+            if i == m:
+                return n - j
+            if j == n:
+                return m - i
+            if (i, j) in dp:
+                return dp[(i, j)]
+            if word1[i] == word2[j]:
+                dp[(i, j)] = dfs(i + 1, j + 1)
+                return dp[(i, j)]
+            res = min(dfs(i + 1, j), dfs(i, j + 1))
+            res = min(res, dfs(i + 1, j + 1))
+            dp[(i, j)] = res + 1
+            return dp[(i, j)]
+        return dfs(0, 0)
