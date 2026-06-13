c7f5a72705cadb8a861f92b7b1b1345f54ced1a0
Aryan Marthak <aryan.marthak1@gmail.com>
2026-06-13 11:47:06 +0530

    Update 567.py

--- MEDIUM/567.py
+++ MEDIUM/567.py
@@ -36,3 +36,26 @@ class Solution:
             if sorted(s2[i:i + len(s1)]) == s1:
                 return True
         return False
+
+# OTIMIMAL Approach (sliding window)
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+        
+        c1 = [0] * 26
+        c2 = [0] * 26
+        
+        for i in range(len(s1)):
+            c1[ord(s1[i]) - ord('a')] += 1
+            c2[ord(s2[i]) - ord('a')] += 1
+
+        if c1 == c2:
+            return True
+
+        for i in range(len(s1), len(s2)):
+            c2[ord(s2[i]) - ord('a')] += 1
+            c2[ord(s2[i - len(s1)]) - ord('a')] -= 1
+            if c1 == c2:
+                return True
+        return False
