a416b4a9139f78f31106e9a67dd9bbba61cb82c6
Aryan Marthak <aryan.marthak1@gmail.com>
2026-06-17 10:32:44 +0530

    Update 739.py

--- MEDIUM/739.py
+++ MEDIUM/739.py
@@ -2,6 +2,20 @@
 
 # Given an array of integers temperatures represents the daily temperatures, return an array answer such that answer[i] is the number of days you have to wait after the ith day to get a warmer temperature. If there is no future day for which this is possible, keep answer[i] == 0 instead.
 
+# BRUTE FORCE
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        res = []
+        for i in range(len(temperatures)):
+            for j in range(i + 1, len(temperatures)):
+                if temperatures[j] > temperatures[i]:
+                    res.append(j - i)
+                    break
+            else:
+                res.append(0)
+        return res
+
+
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         res = [0] * len(temperatures)
