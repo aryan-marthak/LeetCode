9c892b0e71ba2ffc3fd05154b81659237e90f41b
Aryan Marthak <aryan.marthak1@gmail.com>
2026-06-23 12:20:04 +0530

    Update 90.py

--- MEDIUM/90.py
+++ MEDIUM/90.py
@@ -19,3 +19,33 @@ class Solution:
             for j in range(start, len(res)):
                 res.append(res[j] + [nums[i]])
         return res
+    
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res = []
+
+        def bt(i, curr):
+            if i == len(nums):
+                res.append(curr.copy())
+                return
+            curr.append(nums[i])
+            bt(i + 1, curr)
+            curr.pop()
+            while i + 1 < len(nums) and nums[i] == nums[i + 1]:
+                i += 1
+            bt(i + 1, curr)
+        bt(0, [])
+        return res
+    
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res = {()}
+
+        for i in nums:
+            temp = []
+            for j in res:
+                temp.append(j + (i,))
+            res.update(temp)
+        return[list(i) for i in res]
