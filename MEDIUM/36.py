d8535eb8be6876c3e5b172601d16321b6f1eefa1
Aryan Marthak <aryan.marthak1@gmail.com>
2026-06-29 15:10:42 +0530

    36. Valid Sudoku

---
+++ MEDIUM/36.py
@@ -0,0 +1,50 @@
+# 36. Valid Sudoku
+
+# Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+#     Each row must contain the digits 1-9 without repetition.
+#     Each column must contain the digits 1-9 without repetition.
+#     Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+
+# Note:
+
+#     A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+#     Only the filled cells need to be validated according to the mentioned rules.
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        for i in range(9):
+            s = set()
+            for j in range(9):
+                item = board[i][j]
+                if item in s:
+                    return False
+                elif item != '.':
+                    s.add(item)
+        
+        for i in range(9):
+            s = set()
+            for j in range(9):
+                item = board[j][i]
+                if item in s:
+                    return False
+                elif item != '.':
+                    s.add(item)
+            
+        starts = [(0, 0), (0, 3), (0, 6),
+                  (3, 0), (3, 3), (3, 6),
+                  (6, 0), (6, 3), (6, 6)]
+        
+        for i, j in starts:
+            s = set()
+            for row in range(i, i+3):
+                for col in range(j, j+3):
+                    item = board[row][col]
+                    if item in s:
+                        return False
+                    elif item != '.':
+                        s.add(item)
+        return True
+
+# Time Complexity: O(n^2)
+# Space Complexity: O(n)
