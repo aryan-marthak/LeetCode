24d8d90ae3502c40a975ddea0d753336835fd5d8
Aryan Marthak <aryan.marthak1@gmail.com>
2026-06-29 13:12:06 +0530

    bucket sort

--- MEDIUM/347.py
+++ MEDIUM/347.py
@@ -15,3 +15,38 @@ class Solution:
             if len(heap) > k:
                 heapq.heappop(heap)
         return [i[1] for i in heap]
+
+# Sorting
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        for i in nums:
+            count[i] = 1 + count.get(i, 0)
+
+        arr = []
+        for i, j in count.items():
+            arr.append([j, i])
+        arr.sort()
+
+        res = []
+        while len(res) < k:
+            res.append(arr.pop()[1])
+        return res
+
+# Bucket Sort
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
+
+        for num in nums:
+            count[num] = 1 + count.get(num, 0)
+        for num, cnt in count.items():
+            freq[cnt].append(num)
+
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for num in freq[i]:
+                res.append(num)
+                if len(res) == k:
+                    return res
