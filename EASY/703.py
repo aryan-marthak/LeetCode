1f7953d4dcd59fb8370b12fc80bb2cb471090156
Aryan Marthak <aryan.marthak1@gmail.com>
2026-07-02 11:06:00 +0530

    703. Kth Largest Element in a Stream

---
+++ EASY/703.py
@@ -0,0 +1,44 @@
+# 703. Kth Largest Element in a Stream
+
+# You are part of a university admissions office and need to keep track of the kth highest test score from applicants in real-time. This helps to determine cut-off marks for interviews and admissions dynamically as new applicants submit their scores.
+
+# You are tasked to implement a class which, for a given integer k, maintains a stream of test scores and continuously returns the kth highest test score after a new score has been submitted. More specifically, we are looking for the kth highest score in the sorted list of all scores.
+
+# Implement the KthLargest class:
+
+#     KthLargest(int k, int[] nums) Initializes the object with the integer k and the stream of test scores nums.
+#     int add(int val) Adds a new test score val to the stream and returns the element representing the kth largest element in the pool of test scores so far.
+
+# HEAP
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        self.minheap = nums
+        heapq.heapify(self.minheap)
+        while len(self.minheap) > k:
+            heapq.heappop(self.minheap)
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.minheap, val)
+        if len(self.minheap) > self.k:
+            heapq.heappop(self.minheap)
+        return self.minheap[0]
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
+
+# BRUTE FORCE 
+class KthLargest:
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        self.arr = nums
+
+    def add(self, val: int) -> int:
+        self.arr.append(val)
+        self.arr.sort()
+        return self.arr[len(self.arr) - self.k]
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
